6516ade13cb866082fc860c3f9e73874056c1b66
Francisco Mora <francisco.m.collao@gmail.com>
2021-03-29 18:58:46 -0300

    Se actualiza core\Config.php

--- core/Config.php
+++ core/Config.php
@@ -1,6 +1,3 @@
 <?php declare(strict_types=1);
 
-$dsn = 'mysql:host='.$_ENV['DB_HOST'].';port='.$_ENV['DB_PORT'].';dbname='.$_ENV['DB_NAME'].';charset='.$_ENV['DB_CHARSET'];
-$user = $_ENV['DB_USER'];
-$pass = $_ENV['DB_PASS'];
 Flight::set('flight.base_url', 'http://127.0.0.1:8888/');
