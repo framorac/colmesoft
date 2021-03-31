925ab1efca0379ebf911e3514901b86e88af2241
Francisco Mora <francisco.m.collao@gmail.com>
2021-03-31 16:03:01 -0300

    Se agrega bulma css y se genera CSRF Token

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php declare(strict_types=1);
+session_start();
+
+/*
+* CSRF TOKEN
+*/
+$_SESSION['token'] = bin2hex(random_bytes(32));
+$_SESSION['token-expire'] = time() + 3600;
 
 require_once dirname(__DIR__) . '/core/Bootstrap.php';
